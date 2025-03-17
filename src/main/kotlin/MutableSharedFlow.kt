c58c78ad9bf481057d45442df7d696d5613bc876
Đậu Đức Quân <ducquandau03@gmail.com>
2025-03-17 18:12:44 +0700

    tryEmit

--- src/main/kotlin/MutableSharedFlow.kt
+++ src/main/kotlin/MutableSharedFlow.kt
@@ -1,81 +1,38 @@
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
 import kotlin.time.Duration.Companion.seconds
 
 fun main(): Unit = runBlocking {
     val scope = CoroutineScope(Dispatchers.Default)
     val mutableStateFlow = MutableSharedFlow<Int>(
-        replay = 1
+        replay = 0,
+        extraBufferCapacity = 8,
+        onBufferOverflow = BufferOverflow.SUSPEND
     )
 
-    mutableStateFlow.onEach {
-        println("[1] Received: $it")
-    }.launchIn(scope)
-
-    scope.launch {
-        delay(500)
-        repeat(10) {
-            mutableStateFlow.emit(it)
-        }
-        println("Emit 10 items done")
-    }
-
-    delay(2.seconds)
-
     mutableStateFlow
-        .onStart {
-            println("[2] Start collecting...")
-        }
         .onEach {
-            println("[2] Received: $it")
+            println("[1] Received: $it")
         }
         .launchIn(scope)
 
-    delay(100)
+    mutableStateFlow
+        .onEach {
+            println("[2] Received: $it")
+            delay(5.seconds)
+        }
+        .launchIn(scope)
 
-    repeat(10) {
-        mutableStateFlow.emit(it + 10)
+    repeat(30) {
+        println(">>>> Sending: $it")
+        println(mutableStateFlow.tryEmit(it))
+        println("<<<< Sent: $it")
     }
 
     delay(30.seconds)
     scope.cancel()
     println("Done")
 }
-
-/*
-[1] Received: 0
-[1] Received: 1
-[1] Received: 2
-[1] Received: 3
-[1] Received: 4
-[1] Received: 5
-[1] Received: 6
-[1] Received: 7
-[1] Received: 8
-[1] Received: 9
-Emit 10 items done
-[2] Start collecting...
-[2] Received: 9
-[1] Received: 10
-[1] Received: 11
-[2] Received: 10
-[2] Received: 11
-[2] Received: 12
-[1] Received: 12
-[1] Received: 13
-[2] Received: 13
-[2] Received: 14
-[1] Received: 14
-[2] Received: 15
-[1] Received: 15
-[2] Received: 16
-[1] Received: 16
-[1] Received: 17
-[1] Received: 18
-[2] Received: 17
-[2] Received: 18
-[2] Received: 19
-[1] Received: 19*/

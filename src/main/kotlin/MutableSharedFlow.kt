ff7547bdab6a1084af6668b21b2983cfb745332c
Đậu Đức Quân <ducquandau03@gmail.com>
2025-03-17 15:32:47 +0700

    MutableSharedFlow SUSPEND

---
+++ src/main/kotlin/MutableSharedFlow.kt
@@ -0,0 +1,40 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlin.time.Duration.Companion.seconds
+
+fun main(): Unit = runBlocking {
+    val scope = CoroutineScope(Dispatchers.Default)
+    val mutableStateFlow = MutableSharedFlow<Int>(
+        replay = 0,
+        extraBufferCapacity = 8,
+        onBufferOverflow = BufferOverflow.SUSPEND
+    )
+
+    mutableStateFlow
+        .onEach {
+            println("[1] Received: $it")
+        }
+        .launchIn(scope)
+
+    mutableStateFlow
+        .onEach {
+            println("[2] Received: $it")
+            delay(5.seconds)
+        }
+        .launchIn(scope)
+
+    scope.launch {
+        repeat(30) {
+            println(">>>> Sending: $it")
+            mutableStateFlow.emit(it)
+            println("<<<< Sent: $it")
+        }
+    }
+
+    delay(30.seconds)
+    scope.cancel()
+    println("Done")
+}
